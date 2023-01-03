8d7f28db93b7b932ff4fe73a221c031e048426bc
jorislimonier <joris.limonier@hotmail.fr>
2023-01-03 15:18:03 +0100

    Write parsed data to csv

--- src/parse_chinese.py
+++ src/parse_chinese.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from deep_translator import GoogleTranslator  # type: ignore
 from pypinyin import pinyin  # type: ignore
 
@@ -5,12 +6,15 @@ from pypinyin import pinyin  # type: ignore
 class ChineseParser:
   def __init__(self, vocab: list[list[str]]) -> None:
     self.vocab = vocab
+    self._set_translations()
+    self._set_pinyin()
 
-  def set_translations(self) -> None:
+  def _set_translations(self) -> None:
     trans = GoogleTranslator(source="zh-CN", target="en")
     self.translation = trans.translate_batch(self.vocab)
+    self.translation = [t.capitalize() for t in self.translation]
 
-  def set_pinyin(self) -> None:
+  def _set_pinyin(self) -> None:
     vocab_pinyin = []
 
     # Iterate over propositions
@@ -38,5 +42,15 @@ class ChineseParser:
 
       self.pinyin = vocab_pinyin
 
-  def make_df(self) -> None:
-    ...
+  def make_df(self) -> pd.DataFrame:
+    results = pd.DataFrame(
+      data={
+        "chinese": self.vocab,
+        "pinyin": self.pinyin,
+        "translation": self.translation,
+        "sound": None,
+      }
+    )
+    ordered_col_names = ["chinese", "pinyin", "translation", "sound"]
+    results = results[ordered_col_names]
+    return results
